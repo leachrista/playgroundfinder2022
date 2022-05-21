edf82a19c86d3a23aa2fdd7bb7906cea306ab158
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2022-05-21 13:45:51 +0200

    Version 0.001d

--- Files/main.js
+++ Files/main.js
@@ -1,8 +1,12 @@
+function getPgDataFromServer() {
+    fetch()
+}
+
+
 function displayPlaygrounds(pgData) {
     let pTest = document.querySelector("main p");
     pTest.textContent=pgData.toString();
     let pgList = document.querySelector("main ul");
-    //const pgData = ;
     for (let i = 0; i < 10; i++) {
         let pgLi = document.createElement("li");
         pgLi.textContent = pgData.features[i].properties.ANL_NAME;
