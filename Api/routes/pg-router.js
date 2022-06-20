fb08b0ad2ee5b516e0217e8e4c9dc080de3b5768
Hanaa Alraei <hanaa.alraei@stud.fh-campuswien.ac.at>
2022-06-20 12:39:55 +0200

    routers change from Ramon und Hanaa cause login

--- Api/routes/pg-router.js
+++ Api/routes/pg-router.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const controller = require('../controllers/pg-controller');
+const loginController = require('../controllers/login-controller.js');
 
-const routes = Router();
+const routes = Router(); 
 
 routes.get('/pg-data', controller.getPgData);
 routes.get('/playgrounds/:id', controller.getPgDetails);
+routes.post('/login',loginController.login);
 
 module.exports = routes;
