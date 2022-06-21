650f057cad92253c3dd9f2d0a4ea3210784e2711
Florian Bodner <florian.bodner@stud.fh-campuswien.ac.at>
2022-06-21 23:10:06 +0200

    review system - put, post, delete

--- Api/controllers/pg-controller.js
+++ Api/controllers/pg-controller.js
@@ -6,10 +6,24 @@ class PgController {
     }
 
     getPgDetails(req, res) {
-        console.log(req.params);
+        console.log("pg details request at controller with: " + req.params);
         res.send(model.getPgDetails(req.params.pgId));
     }
 
+    postReview(req, res) {
+        console.log("post review request at controller with: " + req.params);
+        res.send(model.postReview(req, res));
+    }
+
+    editReview(req, res) {
+        console.log("edit review request at controller with: " + req.params);
+        res.send(model.editReview(req, res));
+    }
+
+    deleteReview(req, res) {
+        console.log("delete review request at controller with: " + req.params);
+        res.send(model.deleteReview(req, res));
+    }
 }
 
 module.exports = new PgController();
